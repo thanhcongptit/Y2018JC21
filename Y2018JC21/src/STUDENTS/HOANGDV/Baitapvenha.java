a4ae885cbd8db5b8219fb1d8b2bd45847f7f0e83
Do Hoang <dvhoang23@gmail.com>
2018-03-15 10:53:57 +0700

    Them dieu kien IF

--- Y2018JC21/src/STUDENTS/HOANGDV/Baitapvenha.java
+++ Y2018JC21/src/STUDENTS/HOANGDV/Baitapvenha.java
@@ -1,11 +1,12 @@
 package HOANGDV;
+
 import java.util.Scanner;
 
 public class Baitapvenha {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("BaiTap: Tinh Chu vi vs Dien Tich Hinh Chu Nhat\n");
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Nhap vao chieu dai: ");
 		double dai = scan.nextDouble();
@@ -14,10 +15,15 @@ public class Baitapvenha {
 
 		System.out.println("Chieu dai hcn la: " + dai);
 		System.out.println("Chieu dai hcn la: " + rong);
-		System.out.println("Cu vi hcn la: " + (dai + rong));
-		System.out.println("Dien tich hcn la: " + (dai * rong));
-		
-		//Close Scannerdfgdfg
+
+		if (dai > 0 && rong > 0) {
+			System.out.println("Chu vi hcn la: " + (dai + rong) * 2);
+			System.out.println("Dien tich hcn la: " + (dai * rong));
+		} else {
+			System.out.println("Vui long nhap lai chieu dai hoac rong");
+		}
+
+		// Close
 		scan.close();
 	}
 }
