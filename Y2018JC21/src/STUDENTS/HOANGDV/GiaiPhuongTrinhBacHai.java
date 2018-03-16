17472535407c09fe139fd58ca74ed753cb9440a9
Do Hoang <dvhoang23@gmail.com>
2018-03-16 20:24:54 +0700

    Giai PTBH

--- Y2018JC21/src/STUDENTS/HOANGDV/GiaiPhuongTrinhBacHai.java
+++ Y2018JC21/src/STUDENTS/HOANGDV/GiaiPhuongTrinhBacHai.java
@@ -23,11 +23,14 @@ public class GiaiPhuongTrinhBacHai {
 				System.out.println("PT vo nghiem");
 			} else if (dt == 0) {
 				System.out.println("PT co nghiem kep");
-				System.out.println("x=" + (-b / 2 * a));
+				int x = (-b / 2 * a);
+				System.out.println("x=" + x);
 			} else {
 				System.out.println("PT co nghiem phan biet");
-				System.out.println("x1 = " + ((-b + Math.sqrt(dt)) / (2 * a)));
-				System.out.println("x2 = " + ((-b - Math.sqrt(dt)) / (2 * a)));
+				double x1 = ((-b + Math.sqrt(dt)) / (2 * a));
+				System.out.println("x1 = " + x1);
+				double x2 = ((-b + Math.sqrt(dt)) / (2 * a));
+				System.out.println("x2 = " + x2);
 			}
 		}
 	}
