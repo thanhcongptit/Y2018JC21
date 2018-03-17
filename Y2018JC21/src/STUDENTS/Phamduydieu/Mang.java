2836c6a37729e9e6bf28d379d0677c273e8e5338
Duy Diệu <Duy Diu@DESKTOP-THNIB20>
2018-03-17 21:52:53 +0700

    Bài ví dụ 2

--- Y2018JC21/src/STUDENTS/Phamduydieu/Mang.java
+++ Y2018JC21/src/STUDENTS/Phamduydieu/Mang.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Mang {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-// Mảng là kiểu mà chứa các biến cùng loại dữ liệu
-		// Mảng là biến kiểu tham chiếu
-		//giá trị defaul là 0
-		int mang[] = new int[5];
-		Scanner sc == new Scanner(System.in);
-		System.out.println("Nhập phần tử 1:");
-		mang[0] = sc.nextInt();
+		// VÍ DỤ 2: Khai báo 1 mảng số thực gồm n phần tử từ bàn phím sau đó in ra màn hình.
+				Scanner sc = new Scanner(System.in);
+				System.out.print("Nhập số phần tử của mảng: ");
+				int n=sc.nextInt();
+				float a[] = new float[n];
+				for(int i=0; i<n; i++) {
+					System.out.print("Nhập giá trị phần tử thứ "+ (i+1)+": ");
+					a[i]=sc.nextFloat();
+				}
+				for(int i=0;i<n;i++) {
+					System.out.println("Phần tử thứ " + (i+1) + ": " + a[i]);
+				}
 // 
 	}
 
