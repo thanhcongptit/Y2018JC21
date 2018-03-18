4510e5ba12aef5ae5218313c19589a2c02713263
Duy Diệu <Duy Diu@DESKTOP-THNIB20>
2018-03-18 22:21:17 +0700

    mang

---
+++ Y2018JC21/src/STUDENTS/Phamduydieu/BTVN_Mang_16_03.java
@@ -0,0 +1,21 @@
+package Phamduydieu;
+
+import java.util.Scanner;
+
+public class BTVN_Mang_16_03 {
+
+	public static void main(String[] args) {
+/*
+  BTVN: Tim phan lon thu 2 trong 10 phan tu cua mang ma minh nhap vao
+ */
+		int a[] = new int a[10];
+	 Scanner sc = new Scanner(System.in);
+	 for(int i=0; i<10; i++)
+	 {
+		 System.out.println("Nhập giá trị phần tử a"+i+" : ");
+		 a[i] = sc.nextFloat();
+	 }
+		
+	}
+
+}
