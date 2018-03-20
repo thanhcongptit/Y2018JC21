e08a0888739c96c3587d480b555401db3bcc6b7c
Duy Diệu <Duy Diu@DESKTOP-THNIB20>
2018-03-20 21:23:26 +0700

    Đã xong

--- Y2018JC21/src/STUDENTS/Phamduydieu/BTVN_DangKyKetBan.java
+++ Y2018JC21/src/STUDENTS/Phamduydieu/BTVN_DangKyKetBan.java
@@ -7,29 +7,29 @@ public class BTVN_DangKyKetBan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println(" MỜI BẠN NHẬP THÔNG TIN ĐĂNG KÝ:\n(Họ và tên viết liền không dấu.)");
-		System.out.print("Họ Và Tên: ");
-		String HoVaTen = sc.next();	
-		System.out.print("Tuổi: ");
+		System.out.println("MỜI BẠN NHẬP THÔNG TIN ĐĂNG KÝ:");
+		System.out.print("Họ Và Tên:               ");
+		String HoVaTen = sc.nextLine();	
+		System.out.print("Tuổi:                    ");
 		int Tuoi = sc.nextInt();
-		System.out.print("Giới Tính: ");
-		String GioiTinh = sc.next();
-		System.out.print("Tình Trạng Hôn Nhân: \n [1] Chưa có gia đình \n [2] Ly Hôn \n [3] Đã Lập Gia Đình");
+		System.out.print("Giới Tính: \n[1]: Nam\n[2]: Nữ                  ");
+		int GioiTinh = sc.nextInt();
+		System.out.print("Tình Trạng Hôn Nhân: \n [1] Chưa có gia đình \n [2] Ly Hôn \n [3] Đã Lập Gia Đình     ");
 		int TinhTrangHonNhan = sc.nextInt();
 		if (Tuoi > 19)
 		{
 			switch (GioiTinh)
 			{
-			case "Nam":
+			case 1:
 				if(Tuoi>=22)
 				{									
 					switch (TinhTrangHonNhan)
 					{
 					case 1:
-					System.out.println("Bạn đã đăng ký tham gia thành công..!");
+					System.out.println("Chúc mừng bạn "+ HoVaTen + "\nBạn đã đăng ký tham gia thành công..!");
 					break;
 					case 2:
-					System.out.println("Bạn đã đăng ký tham gia thành công..!");
+					System.out.println("Chúc mừng bạn "+ HoVaTen + "\nBạn đã đăng ký tham gia thành công..!");
 					break;
 					default :
 					System.out.println("Bạn không thuộc đối tượng tham gia..!\nYêu cầu chưa có gia đình hoặc đã ly hôn..!");	
@@ -38,16 +38,16 @@ public class BTVN_DangKyKetBan {
 				else
 					System.out.println("Bạn không thuộc đối tượng tham gia..!\nYêu cầu nam lớn hơn 22 tuổi..!");
 			break;
-			case "Nữ":
+			case 2:
 					if(Tuoi>=19)
 					{									
 						switch (TinhTrangHonNhan)
 						{
 						case 1:
-						System.out.println("Bạn đã đăng ký tham gia thành công..!");
+						System.out.println("Chúc mừng bạn "+ HoVaTen + "\nBạn đã đăng ký tham gia thành công..!");
 						break;
 						case 2:
-						System.out.println("Bạn đã đăng ký tham gia thành công..!");
+						System.out.println("Chúc mừng bạn "+ HoVaTen + "\nBạn đã đăng ký tham gia thành công..!");
 						break;
 						default :
 						System.out.println("Bạn không thuộc đối tượng tham gia..!\nĐối tượng tham gia đã ly hôn hoặc chưa có gia đình..!");	
