1e9a4cca4afa818bb1e0a065bc45f3d7688aec9f
Do Hoang <dvhoang23@gmail.com>
2018-03-21 10:27:12 +0700

    BTVN Bai 4 Buoi 4

---
+++ Y2018JC21/src/STUDENTS/HOANGDV/BTVN/Buoi4Bai4.java
@@ -0,0 +1,35 @@
+package HOANGDV.BTVN;
+
+import java.util.Scanner;
+
+public class Buoi4Bai4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bai Tap kiem tra thong tin nguoi dung");
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Nhap Ten: ");
+		String ten = scan.nextLine();
+		System.out.println("Nhap Tuoi: ");
+		String tuoi = scan.nextLine();
+		System.out.println("Nhap Gioi Tinh: ");
+		String gioiTinh = scan.nextLine();
+		System.out.println("Nhap Tinh Trang Hon Nhan: ");
+		String honNhan = scan.nextLine();
+		if (ten.equals("") || tuoi.equals("") || gioiTinh.equals("") || honNhan.equals("")) {
+			System.out.println("Nhap thieu du lieu");
+		} else {
+			if (gioiTinh.equals("Nam") && Integer.parseInt(tuoi) >= 22 && honNhan.equals("Doc than")) {
+				System.out.println("Ban duoc tham gia");
+			} else if (gioiTinh.equals("Nu") && Integer.parseInt(tuoi) >= 19 && honNhan.equals("Doc than")) {
+				System.out.println("Ban duoc tham gia");
+			} else {
+				System.out.println("Ban ko duoc tham gia");
+			}
+		}
+		scan.close();
+	}
+
+}
