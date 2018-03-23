50f325d6e6e73ce0c45af7500983c5a54f710de0
Duy Diệu <Duy Diu@DESKTOP-THNIB20>
2018-03-23 20:57:32 +0700

    abc

---
+++ Y2018JC21/src/STUDENTS/Phamduydieu/BTVN_Pending.java
@@ -0,0 +1,15 @@
+package Phamduydieu;
+
+public class BTVN_Pending {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ Viết chương trình nhập mảng số nguyên từ bàn phím
+ + Sắp xếp và xuất mảng vừa nhập ra màn hình.
+ + Xuất phần tử có giá trị nhỏ nhất ra màn hình.
+ + Tính và xuất ra màn hình trùng binhfc cộng các phần tử chia hết cho 3.
+ */
+	}
+
+}
