1afcab0b5dab1632953717a3fda02bb8b5e6b42b
(ECO) Ha Minh Tien <(ECO) Ha Minh Tien@DESKTOP-0K3ENPB>
2018-03-28 13:48:45 +0700

    bai tap b5

--- Y2018JC21/src/STUDENTS/Tien/B5/Main.java
+++ Y2018JC21/src/STUDENTS/Tien/B5/Main.java
@@ -4,10 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 	// đóng gói
-	
+	// tạo hàm constructor
+	   public Main() {}
 	private String tensp;
+	public Main(String tensp, double dongia, double giamgia, double thuenk) {
+		super();
+		this.tensp = tensp;
+		this.dongia = dongia;
+		this.giamgia = giamgia;
+		this.thuenk = thuenk;
+	}
+	public Main(String tensp, double dongia) {
+		super();
+		this.tensp = tensp;
+		this.dongia = dongia;
+	}
+	
+	public Main(String tensp, double dongia, double giamgia) {
+		super();
+		this.tensp = tensp;
+		this.dongia = dongia;
+		this.giamgia = giamgia;
+		
+	}
 	private double dongia;
 	private double giamgia;
+	private double thuenk;
+	public double getThuenk() {
+		return thuenk;
+	}
+	public void setThuenk(double thuenk) {
+		this.thuenk = thuenk;
+	}
 	public String getTensp() {
 		return tensp;
 	}
@@ -26,15 +54,7 @@ public class Main {
 	public void setGiamgia(double giamgia) {
 		this.giamgia = giamgia;
 	}
-	// tạo hàm constructor
-	public Main(String tensp, double dongia, double giamgia) {
-		super();
-		this.tensp = tensp;
-		this.dongia = dongia;
-		this.giamgia = giamgia;
-	}
-	// tạo hàm constructor
-   public Main() {}
+	
    // nhap du liệu sản phẩm từ bàn phím
    public void nhapdulieu() {
 	   Scanner nhap =new Scanner(System.in);
@@ -43,12 +63,23 @@ public class Main {
 	   System.out.print("donn gia  : ");
 	   dongia=nhap.nextDouble();
 	   System.out.print("giam gia : ");
-	   giamgia=nhap.nextDouble();
-	   double thuenk=(dongia-giamgia)*10/100;
-	   System.out.println("thue nhap khau: "+ thuenk);
-	   System.out.println("             ");
-	   System.out.println("             ");
-	
+	   giamgia=nhap.nextDouble();	  
    }
+   public void nhapdulieu1() {
+	   Scanner nhap =new Scanner(System.in);
+	   System.out.print("ten san pham : ");
+	   tensp=nhap.nextLine();
+	   System.out.print("donn gia  : ");
+	   dongia=nhap.nextDouble();
+   }
+   public void xuat () {
+	   thuenk=(dongia-giamgia)*0.1;
+	   System.out.println("ten san pham: "+ tensp + "      "+"don gia: "+ dongia + "      "+"giam gia: "+giamgia
+			   +"      "+ "thuenk:"+ thuenk);
+	   System.out.println("         ");
+	   System.out.println("         ");
+   }
+
+   
    
 }
--- Y2018JC21/src/STUDENTS/Tien/B5/sanpham.java
+++ Y2018JC21/src/STUDENTS/Tien/B5/sanpham.java
@@ -6,12 +6,19 @@ public class sanpham {
 		// TODO Auto-generated method stub
 	Main sp1=new Main();
 	System.out.println("nhap du lieu san pham 1 : ");
+	System.out.println ("       ");
 	sp1.nhapdulieu();
-	
+	sp1.xuat();
 	Main sp2=new Main();
 	System.out.println("nhap du lieu san pham 2 : ");
+	System.out.println ("       ");
 	sp2.nhapdulieu();
+	sp2.xuat();
 	
+	Main sp3=new Main("sp3",100);
+	sp3.xuat();
+	
+	Main sp4=new Main("sp4",100,20);
+	sp4.xuat();
 	}
-
 }
