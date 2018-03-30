2aa02c8b3d4d73a01231a585e6d99ec7da09e0dc
dokhactu <dokhactu17493@gmail.com>
2018-03-30 12:29:13 +0700

    BTVN 28_03_2018

---
+++ Y2018JC21/src/STUDENTS/DoKhacTu/TaiLieu.java
@@ -0,0 +1,28 @@
+package STUDENTS.DoKhacTu;
+
+public class TaiLieu {
+	private String maTaiLieu;
+	private String tenNhaSx;
+	private int soBanPhatHanh;
+	public String getMaTaiLieu() {
+		return maTaiLieu;
+	}
+	public void setMaTaiLieu(String maTaiLieu) {
+		this.maTaiLieu = maTaiLieu;
+	}
+	public String getTenNhaSx() {
+		return tenNhaSx;
+	}
+	public void setTenNhaSx(String tenNhaSx) {
+		this.tenNhaSx = tenNhaSx;
+	}
+	public int getSoBanPhatHanh() {
+		return soBanPhatHanh;
+	}
+	public void setSoBanPhatHanh(int soBanPhatHanh) {
+		this.soBanPhatHanh = soBanPhatHanh;
+	}
+	
+	
+
+}
